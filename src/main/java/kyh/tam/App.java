ccb6999ebe493c3e25b18606e0b511abdd8a426a
kyh <yh0921k@gmail.com>
2019-12-20 14:33:51 +0900

    update v09

--- src/main/java/kyh/tam/App.java
+++ src/main/java/kyh/tam/App.java
@@ -6,43 +6,44 @@ import java.text.SimpleDateFormat;
 import java.util.Date;
 
 public class App {   
+  static BufferedReader br = new BufferedReader(new InputStreamReader(System.in)); 
+  static final int STUFF_SIZE  = 100;
+  static final int MEMBER_SIZE = 100;
+  static final int BOARD_SIZE  = 100;
+  static int stuffCnt  = 0;
+  static int memberCnt = 0;      
+  static int boardCnt  = 0;      
+  
+  static class Stuff {
+    int stuffNum;
+    String stuffName;
+    String stuffContents;
+    String seller;
+    String category;
+    int price;
+  }
+  static class Member {
+    int personNum;
+    String personName;
+    String email;
+    String address;
+    String password;
+    String picture;
+    String phoneNum;
+    Date registeredDate;
+  }
+  static class Board {
+    int postNum;
+    String detail;
+    Date writeDate;
+    int viewCount;
+  }
+  static Board[]  boards  = new Board[BOARD_SIZE];
+  static Member[] members = new Member[MEMBER_SIZE]; 
+  static Stuff[]  stuffs  = new Stuff[STUFF_SIZE];
+  static String   command;
+  
   public static void main(String[] args) throws Exception {
-    BufferedReader br = new BufferedReader(new InputStreamReader(System.in)); 
-    final int STUFF_SIZE  = 100;
-    final int MEMBER_SIZE = 100;
-    final int BOARD_SIZE  = 100;
-    int stuffCnt  = 0;
-    int memberCnt = 0;      
-    int boardCnt  = 0;      
-
-    class Stuff {
-      int stuffNum;
-      String stuffName;
-      String stuffContents;
-      String seller;
-      String category;
-      int price;
-    }
-    class Member {
-      int personNum;
-      String personName;
-      String email;
-      String address;
-      String password;
-      String picture;
-      String phoneNum;
-      Date registeredDate;
-    }
-    class Board {
-      int postNum;
-      String detail;
-      Date writeDate;
-      int viewCount;
-    }
-    Board[]  boards  = new Board[BOARD_SIZE];
-    Member[] members = new Member[MEMBER_SIZE]; 
-    Stuff[]  stuffs  = new Stuff[STUFF_SIZE];
-    String   command;
     
     do {
       System.out.printf("-----------------------------------------------------------------------------\n");
@@ -51,72 +52,22 @@ public class App {
 
       switch (command) {
         case "/stuff/add":
-          Stuff s = new Stuff();
-          System.out.printf("-----------------------------------------------------------------------------\n");
-          System.out.printf("번호 : ");
-          s.stuffNum = Integer.parseInt(br.readLine());
-          System.out.printf("물품명 : ");
-          s.stuffName = br.readLine();
-          System.out.printf("판매자 : ");
-          s.seller = br.readLine();
-          System.out.printf("분류 : ");
-          s.category = br.readLine();
-          System.out.printf("설명 : ");
-          s.stuffContents = br.readLine();
-          System.out.printf("가격 : ");
-          s.price = Integer.parseInt(br.readLine());            
-          stuffs[stuffCnt++] = s;
+          addStuff();
           break;
         case "/stuff/list":
-          System.out.printf("-----------------------------------------------------------------------------\n");
-          for(int i=0; i<stuffCnt; i++)      
-            System.out.printf("%s, %s, %s, %s, %s\n", 
-                stuffs[i].stuffNum, stuffs[i].stuffName, stuffs[i].category, stuffs[i].seller, stuffs[i].price);
+          printStuffList();
           break;
         case "/member/add":
-          Member m = new Member();
-          System.out.printf("-----------------------------------------------------------------------------\n");
-          System.out.printf("번호 : ");
-          m.personNum = Integer.parseInt(br.readLine());
-          System.out.printf("이름 : ");
-          m.personName = br.readLine();
-          System.out.printf("Email : ");
-          m.email = br.readLine();
-          System.out.printf("주소 : ");
-          m.address = br.readLine();
-          System.out.printf("암호 : ");
-          m.password = br.readLine();
-          System.out.printf("사진 : ");
-          m.picture = br.readLine();
-          System.out.printf("전화 : ");
-          m.phoneNum = br.readLine();
-          m.registeredDate = new Date();
-          members[memberCnt++] = m;
+          addMember();
           break;
         case "/member/list":
-          System.out.printf("-----------------------------------------------------------------------------\n");
-          for(int i=0; i<memberCnt; i++)      
-            System.out.printf("%s, %s, %s, %s, %s\n", 
-                members[i].personNum, members[i].personName, members[i].address, members[i].phoneNum,
-                new SimpleDateFormat("yyyy-MM-dd").format(members[i].registeredDate));
+          printMemberList();
           break;
         case "/board/add":
-          Board b = new Board();
-          System.out.printf("-----------------------------------------------------------------------------\n");
-          System.out.printf("번호 : ");
-          b.postNum = Integer.parseInt(br.readLine());
-          System.out.printf("내용 : ");
-          b.detail = br.readLine();
-          b.writeDate = new Date();
-          b.viewCount = 0;
-          boards[boardCnt++] = b;
+          addBoard();
           break;
         case "/board/list":
-          System.out.printf("-----------------------------------------------------------------------------\n");
-          for(int i=0; i<boardCnt; i++) { 
-            String wDate = new SimpleDateFormat("yyyy-MM-dd").format(boards[i].writeDate);
-            System.out.printf("%s, %s, %s, %s\n", boards[i].postNum, boards[i].detail, wDate, boards[i].viewCount);
-          }
+          printBoardList();
           break;
         default:
           if (!command.equalsIgnoreCase("quit"))
@@ -124,5 +75,73 @@ public class App {
       }
     } while (!command.equalsIgnoreCase("quit"));
     System.out.println("Bye");
+  }
+  static void addStuff() throws Exception {
+    Stuff s = new Stuff();
+    System.out.printf("-----------------------------------------------------------------------------\n");
+    System.out.printf("번호 : ");
+    s.stuffNum = Integer.parseInt(br.readLine());
+    System.out.printf("물품명 : ");
+    s.stuffName = br.readLine();
+    System.out.printf("판매자 : ");
+    s.seller = br.readLine();
+    System.out.printf("분류 : ");
+    s.category = br.readLine();
+    System.out.printf("설명 : ");
+    s.stuffContents = br.readLine();
+    System.out.printf("가격 : ");
+    s.price = Integer.parseInt(br.readLine());            
+    stuffs[stuffCnt++] = s;
+  }
+  static void printStuffList() {
+    System.out.printf("-----------------------------------------------------------------------------\n");
+    for(int i=0; i<stuffCnt; i++)      
+      System.out.printf("%s, %s, %s, %s, %s\n", 
+          stuffs[i].stuffNum, stuffs[i].stuffName, stuffs[i].category, stuffs[i].seller, stuffs[i].price);
+  }
+  static void addMember() throws Exception {
+    Member m = new Member();
+    System.out.printf("-----------------------------------------------------------------------------\n");
+    System.out.printf("번호 : ");
+    m.personNum = Integer.parseInt(br.readLine());
+    System.out.printf("이름 : ");
+    m.personName = br.readLine();
+    System.out.printf("Email : ");
+    m.email = br.readLine();
+    System.out.printf("주소 : ");
+    m.address = br.readLine();
+    System.out.printf("암호 : ");
+    m.password = br.readLine();
+    System.out.printf("사진 : ");
+    m.picture = br.readLine();
+    System.out.printf("전화 : ");
+    m.phoneNum = br.readLine();
+    m.registeredDate = new Date();
+    members[memberCnt++] = m;
+  }
+  static void printMemberList() {
+    System.out.printf("-----------------------------------------------------------------------------\n");
+    for(int i=0; i<memberCnt; i++)      
+      System.out.printf("%s, %s, %s, %s, %s\n", 
+          members[i].personNum, members[i].personName, members[i].address, members[i].phoneNum,
+          new SimpleDateFormat("yyyy-MM-dd").format(members[i].registeredDate));
+  }
+  static void addBoard() throws Exception {
+    Board b = new Board();
+    System.out.printf("-----------------------------------------------------------------------------\n");
+    System.out.printf("번호 : ");
+    b.postNum = Integer.parseInt(br.readLine());
+    System.out.printf("내용 : ");
+    b.detail = br.readLine();
+    b.writeDate = new Date();
+    b.viewCount = 0;
+    boards[boardCnt++] = b;
+  }
+  static void printBoardList() {
+    System.out.printf("-----------------------------------------------------------------------------\n");
+    for(int i=0; i<boardCnt; i++) { 
+      String wDate = new SimpleDateFormat("yyyy-MM-dd").format(boards[i].writeDate);
+      System.out.printf("%s, %s, %s, %s\n", boards[i].postNum, boards[i].detail, wDate, boards[i].viewCount);
+    }
   }
 }
