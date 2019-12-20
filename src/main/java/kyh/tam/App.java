6fd88c229439e0e722a6cb862426c4f2747617c6
kyh <yh0921k@gmail.com>
2019-12-20 14:24:47 +0900

    update v08

--- src/main/java/kyh/tam/App.java
+++ src/main/java/kyh/tam/App.java
@@ -2,12 +2,18 @@ package kyh.tam;
 
 import java.io.BufferedReader;
 import java.io.InputStreamReader;
+import java.text.SimpleDateFormat;
+import java.util.Date;
 
 public class App {   
   public static void main(String[] args) throws Exception {
     BufferedReader br = new BufferedReader(new InputStreamReader(System.in)); 
-    final int SIZE = 100;
-    int tmpCnt = 0;       
+    final int STUFF_SIZE  = 100;
+    final int MEMBER_SIZE = 100;
+    final int BOARD_SIZE  = 100;
+    int stuffCnt  = 0;
+    int memberCnt = 0;      
+    int boardCnt  = 0;      
 
     class Stuff {
       int stuffNum;
@@ -17,42 +23,106 @@ public class App {
       String category;
       int price;
     }
-    Stuff[] stuffs = new Stuff[SIZE];
-    
-    for(int i = 0; i < SIZE; i++) { 
-      Stuff s = new Stuff();
-      tmpCnt++;
-      System.out.printf("-----------------------------------------------------------------------------\n");
-      System.out.printf("번호 : ");
-      s.stuffNum = Integer.parseInt(br.readLine());
-      System.out.printf("물품명 : ");
-      s.stuffName = br.readLine();
-      System.out.printf("판매자 : ");
-      s.seller = br.readLine();
-      System.out.printf("분류 : ");
-      s.category = br.readLine();
-      System.out.printf("설명 : ");
-      s.stuffContents = br.readLine();
-      System.out.printf("가격 : ");
-      s.price = Integer.parseInt(br.readLine());            
-      stuffs[i] = s;
-      
-      System.out.printf("계속 입력하시겠습니까? (Y/n) : ");
-      String tmp = br.readLine();
-      if(tmp.equalsIgnoreCase("y"))
-        continue;
-      else if(tmp.equalsIgnoreCase("n")) 
-        break;
-      else {
-        System.out.println("잘못 입력하셨습니다.\n프로그램을 종료합니다.\n");
-        return;
-      }
+    class Member {
+      int personNum;
+      String personName;
+      String email;
+      String address;
+      String password;
+      String picture;
+      String phoneNum;
+      Date registeredDate;
     }
+    class Board {
+      int postNum;
+      String detail;
+      Date writeDate;
+      int viewCount;
+    }
+    Board[]  boards  = new Board[BOARD_SIZE];
+    Member[] members = new Member[MEMBER_SIZE]; 
+    Stuff[]  stuffs  = new Stuff[STUFF_SIZE];
+    String   command;
+    
+    do {
+      System.out.printf("-----------------------------------------------------------------------------\n");
+      System.out.printf("\n$ ");
+      command = br.readLine();
 
-    System.out.printf("-----------------------------------------------------------------------------\n");
-    for(int i=0; i<tmpCnt; i++)      
-      System.out.printf("%s, %s, %s, %s, %s\n", 
-          stuffs[i].stuffNum, stuffs[i].stuffName, stuffs[i].category, stuffs[i].seller, stuffs[i].price);
-    System.out.printf("-----------------------------------------------------------------------------\n");
+      switch (command) {
+        case "/stuff/add":
+          Stuff s = new Stuff();
+          System.out.printf("-----------------------------------------------------------------------------\n");
+          System.out.printf("번호 : ");
+          s.stuffNum = Integer.parseInt(br.readLine());
+          System.out.printf("물품명 : ");
+          s.stuffName = br.readLine();
+          System.out.printf("판매자 : ");
+          s.seller = br.readLine();
+          System.out.printf("분류 : ");
+          s.category = br.readLine();
+          System.out.printf("설명 : ");
+          s.stuffContents = br.readLine();
+          System.out.printf("가격 : ");
+          s.price = Integer.parseInt(br.readLine());            
+          stuffs[stuffCnt++] = s;
+          break;
+        case "/stuff/list":
+          System.out.printf("-----------------------------------------------------------------------------\n");
+          for(int i=0; i<stuffCnt; i++)      
+            System.out.printf("%s, %s, %s, %s, %s\n", 
+                stuffs[i].stuffNum, stuffs[i].stuffName, stuffs[i].category, stuffs[i].seller, stuffs[i].price);
+          break;
+        case "/member/add":
+          Member m = new Member();
+          System.out.printf("-----------------------------------------------------------------------------\n");
+          System.out.printf("번호 : ");
+          m.personNum = Integer.parseInt(br.readLine());
+          System.out.printf("이름 : ");
+          m.personName = br.readLine();
+          System.out.printf("Email : ");
+          m.email = br.readLine();
+          System.out.printf("주소 : ");
+          m.address = br.readLine();
+          System.out.printf("암호 : ");
+          m.password = br.readLine();
+          System.out.printf("사진 : ");
+          m.picture = br.readLine();
+          System.out.printf("전화 : ");
+          m.phoneNum = br.readLine();
+          m.registeredDate = new Date();
+          members[memberCnt++] = m;
+          break;
+        case "/member/list":
+          System.out.printf("-----------------------------------------------------------------------------\n");
+          for(int i=0; i<memberCnt; i++)      
+            System.out.printf("%s, %s, %s, %s, %s\n", 
+                members[i].personNum, members[i].personName, members[i].address, members[i].phoneNum,
+                new SimpleDateFormat("yyyy-MM-dd").format(members[i].registeredDate));
+          break;
+        case "/board/add":
+          Board b = new Board();
+          System.out.printf("-----------------------------------------------------------------------------\n");
+          System.out.printf("번호 : ");
+          b.postNum = Integer.parseInt(br.readLine());
+          System.out.printf("내용 : ");
+          b.detail = br.readLine();
+          b.writeDate = new Date();
+          b.viewCount = 0;
+          boards[boardCnt++] = b;
+          break;
+        case "/board/list":
+          System.out.printf("-----------------------------------------------------------------------------\n");
+          for(int i=0; i<boardCnt; i++) { 
+            String wDate = new SimpleDateFormat("yyyy-MM-dd").format(boards[i].writeDate);
+            System.out.printf("%s, %s, %s, %s\n", boards[i].postNum, boards[i].detail, wDate, boards[i].viewCount);
+          }
+          break;
+        default:
+          if (!command.equalsIgnoreCase("quit"))
+            System.out.printf("Incorrect Command\n");
+      }
+    } while (!command.equalsIgnoreCase("quit"));
+    System.out.println("Bye");
   }
 }
