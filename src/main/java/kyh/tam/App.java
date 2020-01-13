b0c1f1a3be360fd995673d705c7d64ddff7e5cf8
kyh <yh0921k@gmail.com>
2020-01-13 17:18:31 +0900

    update v22

--- src/main/java/kyh/tam/App.java
+++ src/main/java/kyh/tam/App.java
@@ -6,12 +6,14 @@ import kyh.tam.handler.BoardHandler;
 import kyh.tam.handler.MemberHandler;
 import kyh.tam.handler.StuffHandler;
 import kyh.util.Prompt;
+import kyh.util.Queue;
 import kyh.util.Stack;
 
 public class App { 
   
   static BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
   static Stack<String> commandStack = new Stack<>();
+  static Queue<String> commandQueue = new Queue<>();
   
   public static void main(String[] args) throws Exception {
     Prompt prompt = new Prompt(br);
@@ -28,6 +30,7 @@ public class App {
         continue;
       
       commandStack.push(command);
+      commandQueue.offer(command);
       
       switch (command) {
         case "/stuff/add":
@@ -78,6 +81,9 @@ public class App {
         case "history":
           printCommandHistory();
           break;
+        case "history2":
+          printCommandHistory2();
+          break;
         default:
           if (!command.equalsIgnoreCase("quit"))
             System.out.printf("Incorrect Command\n");
@@ -92,13 +98,24 @@ public class App {
     int count = 0;
     while(!historyStack.empty()) {
       System.out.println(historyStack.pop());
-      count++;
-      if(count % 5 == 0) {
+      if(++count % 5 == 0) {
         System.out.printf(": ");
         if(br.readLine().equalsIgnoreCase("q"))
           break;
+      }      
+    }
+  }
+  private static void printCommandHistory2() throws Exception {
+    Queue<String> historyQueue = (Queue<String>)commandQueue.clone();
+    int count = 0;
+    while(historyQueue.size() > 0) {
+      System.out.println(historyQueue.poll());     
+      if(++count % 5 == 0) {
+        System.out.printf(": ");
+        
+        if(br.readLine().equalsIgnoreCase("q"))
+          break;
       }
-      
     }
   }
 }
---
+++ src/main/java/kyh/util/Queue.java
@@ -0,0 +1,25 @@
+package kyh.util;
+
+public class Queue<E> extends LinkedList<E> implements Cloneable {
+  
+  public void offer(E value) {
+    this.add(value);
+  }
+  
+  public E poll() {
+    return this.remove(0);
+  }
+  
+  @Override
+  public Queue<E> clone() {
+    try {
+      Queue<E> temp = new Queue<>();
+      for(int i = 0; i < this.size(); i++)
+        temp.offer(this.get(i));
+      return temp;
+    } catch (Exception e) {
+      e.printStackTrace();
+      return null;
+    }
+  }
+}
