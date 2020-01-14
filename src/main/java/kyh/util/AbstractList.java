f9cc822044380c96d99a7a9e66b9d511c3ca6b9b
kyh <yh0921k@gmail.com>
2020-01-14 17:09:33 +0900

    update v23_03

--- src/main/java/kyh/util/AbstractList.java
+++ src/main/java/kyh/util/AbstractList.java
@@ -1,17 +1,9 @@
 package kyh.util;
 
-public abstract class AbstractList<E> {
+public abstract class AbstractList<E> implements List<E> {
   protected int size;
 
   public int size() {
     return size;
   }
-  
-  public abstract void add(E e);
-  public abstract void add(int index, E e);
-  public abstract E get(int index);
-  public abstract E set(int index, E e);
-  public abstract E remove(int index);
-  public abstract Object[] toArray();
-  public abstract E[] toArray(E[] arr);
 }
