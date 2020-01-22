2d74f95d8aff9daaeef669e16e71a7f0d574ae24
kyh <yh0921k@gmail.com>
2020-01-22 10:29:33 +0900

    update v27

--- src/main/java/kyh/tam/App.java
+++ src/main/java/kyh/tam/App.java
@@ -90,7 +90,11 @@ public class App {
 
       Command commandHandler = commandMap.get(command);
       if (commandHandler != null)
-        commandHandler.execute();
+        try {
+          commandHandler.execute();
+        } catch (Exception e) {
+          System.out.printf("명령어 실행 중 오류 발생 : %s\n", e.getMessage());
+        }
       else
         System.out.println("실행할 수 없는 명령입니다.");
 
